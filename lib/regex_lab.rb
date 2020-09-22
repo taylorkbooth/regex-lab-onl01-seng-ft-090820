6e40db3cfe5e115cb10901dde922cc05d8fe9b88
Taylor Booth <taylorkbooth@gmail.com>
2020-09-22 03:12:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,19 +6,23 @@ if word.match(/^[AEIOU]|^[aeiou]/)
   end
 end
 
+
+
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w+ing/)
-
 end
+
+
 
 def words_five_letters_long(text)
   return_array = text.split(" ")
   return_array.grep(/\b\w{5}\b/)
-
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
 
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+ text.
 end
 
 def valid_phone_number?(phone)
